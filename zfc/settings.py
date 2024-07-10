bfc0e2b1c091d7f61869e9ff14a78e3649fe86b2
acarbonilla <carbonillaalfredo@gmail.com>
2024-07-10 11:00:10 +0800

    changing database connection

--- zfc/settings.py
+++ zfc/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'vms3',
-        'USER': 'root',
+        'USER': 'carbonillaat',
         'PASSWORD': '$Everyday23',
         'HOST': 'localhost',
         'PORT': '3306',
